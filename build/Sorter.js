327016325ab23904f5c4d3d1c4b05bd0769b4dc9
nik-rimor <nickkekes@gmail.com>
2024-05-22 11:39:13 +0300

    define Sortable interface

--- build/Sorter.js
+++ build/Sorter.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
+// Define interface on how to be eligible for sorting
 class Sorter {
     constructor(collection) {
         this.collection = collection;
